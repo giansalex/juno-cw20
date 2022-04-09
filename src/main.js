be249ca0fa57aa0b2fd7dc3b5993d31ab4ecb059
Giancarlos Salas <giansalex@gmail.com>
2022-04-09 12:29:52 -0500

    Remove deprecated features (keplr v0.10.0)

--- src/main.js
+++ src/main.js
@@ -225,7 +225,7 @@ async function registerKeplr() {
                         average: 0.025,
                         high: 0.04
                     },
-                    features: ["stargate", 'ibc-transfer', 'cosmwasm', 'no-legacy-stdTx', 'ibc-go']
+                    features: ['ibc-transfer', 'cosmwasm', 'ibc-go']
                 });
 
                 return true;
